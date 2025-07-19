f42b9b3bc1ad67ba4f1e40e17a697b9618cbbf37
afathy <ahmdfthy95@gmail.com>
2025-07-19 13:24:13 +0300

    Initial domain setup

---
+++ TicketManagement.Domain/Entities/Tickets/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketManagement.Domain.Entities.Tickets
+{
+    public class Ticket
+    {
+        public Guid Id { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Governorate { get; private set; }
+        public string City { get; private set; }
+        public string District { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsHandled { get; private set; }
+
+        internal Ticket(string phoneNumber, string governorate, string district, string city, bool isHandled)
+        {
+            Id = Guid.NewGuid();
+            PhoneNumber = phoneNumber;
+            Governorate = governorate;
+            District = district;
+            City = city;
+            CreatedAt = DateTime.UtcNow;
+            IsHandled = false;
+        }
+
+        public void MarkAsHandled()
+        {
+            IsHandled = true;
+        }
+    }
+}
---
+++ TicketManagement.Domain/Repositories/ITicketRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManagement.Domain.Entities.Tickets;
+
+namespace TicketManagement.Domain.Repositories
+{
+    public interface ITicketRepository
+    {
+        Task<Ticket> AddAsync(Claim claim);
+        Task<Ticket?> GetByIdAsync(Guid claimId);
+        Task<IEnumerable<Claim>> GetAllAsync();
+    }
+}
